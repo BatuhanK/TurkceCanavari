593dd5bf044f0ba7b5701b44e76b7e71fdc289b2
Batuhan KATIRCI <katircib@gmail.com>
2014-10-09 11:20:30 +0300

    1-2 basit değişiklik

--- web/js/app.js
+++ web/js/app.js
@@ -1,36 +1,38 @@
-  var socket =  io('188.226.190.175:3333'); // Server adresinizi girin
+  var socket =  io('twikan.com:3333'); // Server adresinizi girin
   var yanlis_count = 0;
 
-  var kelimeler = ["acaip","aferim","anfi","antreman","arasıra","arasöz","asvalt","ardarda","birarada","birşey","deyil","diyil","dinazor","eksoz","egzost","eşortman","eşşek","eylence","felan","farketmek","gurup","halikar","harfiyat","herbiri","hergün","herhangibir","herkez","herşey","hiçkimse","ilkönce","idda","kareografi","makina","malesef","menejer","menacer","metod","meyva","muhattap","müsade","orjinal","ritm","rütuş","sandöviç","sarmısak","sezeryan","süpriz","şartel","vejeteryan","yanlız","yalnış"];
+  var kelimeler = ["bide","acaip","aferim","anfi","antreman","arasıra","arasöz","asvalt","ardarda","birarada","birşey","deyil","diyil","dinazor","eksoz","egzost","eşortman","eşşek","eylence","felan","farketmek","gurup","halikar","harfiyat","herbiri","hergün","herhangibir","herkez","herşey","hiçkimse","ilkönce","idda","kareografi","makina","malesef","menejer","menacer","metod","meyva","muhattap","müsade","orjinal","ritm","rütuş","sandöviç","sarmısak","sezeryan","süpriz","şartel","vejeteryan","yanlız","yalnış"];
 
   socket.on('connect_yanlis', function (data){
     $('#yanlis_sayi').html(data.yanlis_count);
-});
+  });
 
   socket.on('yanlis_yazim', function (data){
     $('#sil').fadeOut();
 
-    yanlis_count++;
     var tweet = data.tweet;
     var user = tweet.user;
     var name = user.name;
     var screen_name = user.screen_name;
     var picture = user.profile_image_url;
-
-    $html = '<li><img class="img-rounded" height="26px" src="'+picture+'" /> <p class="tweet"><span class="screen_name">@'+screen_name+' </span>'+ tweet.text + ' </p></li>';
-    if(yanlis_count>6){
+    if(screen_name.indexOf("TurkceRobotu") == -1){
+      yanlis_count++;
+      $html = '<li><img class="img-rounded" height="26px" src="'+picture+'" /> <p class="tweet"><span class="screen_name">@'+screen_name+' </span>'+ tweet.text + ' </p></li>';
+      if(yanlis_count>6){
         $('.yanlis_liste li:last').fadeOut(300);
         $('.yanlis_liste li:last').remove();
-    }
+      }
 
-    console.log(data);
+      console.log(data);
 
 
-    $($html).hide().prependTo(".yanlis_liste").slideDown(400);
+      $($html).hide().prependTo(".yanlis_liste").slideDown(400);
 
-    for (var i = kelimeler.length - 1; i >= 0; i--) {
+      for (var i = kelimeler.length - 1; i >= 0; i--) {
         $('.yanlis_liste').highlight(kelimeler[i]);
-    };
+      };
 
-    $('#yanlis_sayi').html(data.yanlis_count);
-});
+      $('#yanlis_sayi').html(data.yanlis_count);
+    }
+    
+  });
