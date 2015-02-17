48131419f5b79c2e6cbee1008180dbc949e9bd45
Batuhan KATIRCI <katircib@gmail.com>
2015-02-17 20:47:32 +0200

    2412312
    asdasd

--- http.js
+++ http.js
@@ -3,7 +3,7 @@ module.exports=function() {
 	var connect = require('connect'),
 	    http = require('http'),
 	    directory = 'web',
-	    http_port = 8080;
+	    http_port = 8080; // FAK
 	connect()
 	    .use(connect.static(directory))
 	    .listen(http_port);
